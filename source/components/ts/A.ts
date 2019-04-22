651416923fdae9a6764af63a2c47b552fe011a0a
Ivan <devskyfly@gmail.com>
2019-04-22 17:28:14 +0300

    Add ts

---
+++ source/components/ts/A.ts
@@ -0,0 +1,15 @@
+class A
+{
+    id: number;
+    name: string;
+
+    public printName():void
+    {
+        console.log(this.name);
+    }
+
+    public printId():void
+    {
+        console.log(this.id);
+    }
+}
